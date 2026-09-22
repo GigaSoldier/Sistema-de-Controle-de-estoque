29f74536797264e5dbdb6d0741d208713d81dbed
RianAlencar <marlenealencaradv@gmail.com>
2026-09-22 09:35:36 -0400

    Update estoque.h

---
+++ estoque.h
@@ -0,0 +1,20 @@
+#ifndef ESTOQUE_H
+#define ESTOQUE_H
+
+#define MAX_ITENS 10
+#define TAXA_PADRAO 0.05
+#define TAXA_DESCONTO 0.05
+
+typedef struct {
+    int id;
+    char nome[30];
+    float preco;
+    int quantidade;
+} Produto;
+
+void exibir_menu(void);
+void listar_produtos(Produto lista[], int total);
+float calcular_total(Produto lista[], int total);
+float aplicar_desconto(float total);
+
+#endif
